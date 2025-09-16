57f47d7049997b956bfb11ef57302049a1e3ebf7
Khalid Hossain <86224418+KhalidHossain3108@users.noreply.github.com>
2025-09-16 14:14:58 +0600

    Update cbus_to_io_controller_bridge.v author Infos

--- picosoc_ac_controller/design_data/verilog/cbus_to_io_controller_bridge.v
+++ picosoc_ac_controller/design_data/verilog/cbus_to_io_controller_bridge.v
@@ -1,3 +1,24 @@
+/*
+ *  cbus_to_io_controller_bridge.v -- CBUS to I/O Controller Bridge with PWM and GPIO Support
+ *
+ *  This module bridges a memory-mapped bus to GPIO and PWM outputs.
+ *
+ *  Copyright (C) 2025  AKM Uday Hasan Bhuiyan <udayhasan.eee.buet@gmail.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
 module cbus_to_io_controller_bridge (
     input  wire        clk,
     input  wire        resetn,
@@ -134,4 +155,5 @@ module cbus_to_io_controller_bridge (
             end
         end
     end
+
 endmodule
