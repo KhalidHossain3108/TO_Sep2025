5734c63646680ff5b06a8871c7aea85eb8e9f5c9
KhalidHossain3108 <mdkhalidhossain3108@gmail.com>
2025-09-20 23:03:42 +0600

    Add author/license headers to Verilog modules

--- picosoc_ac_controller/design_data/verilog/cbus_to_uart_bridge.v
+++ picosoc_ac_controller/design_data/verilog/cbus_to_uart_bridge.v
@@ -1,4 +1,21 @@
-// Complete UART Bridge RTL
+/*
+ *  cbus_to_uart_bridge -- Simple Bus to UART Bridge Controller
+ *
+ *  Copyright (C) 2024  AKM Uday Hassan Bhuiyan <udayhasan.eee.buet@gmail.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 module cbus_to_uart_bridge(
     input clk,
     input resetn,
